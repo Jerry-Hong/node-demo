f4c50e27d227539da3932a4f145d92197d1aeee3
JerryHong <s6323859@hotmail.com>
2018-05-15 20:36:48 +0800

    add helmet & serve-static middleware

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const Express = require('express');
+const helmet = require('helmet');
+const serveStatic = require('serve-static');
 
 const app = Express();
 
-app.get('/', function (req, res) {
-  console.log('Hello');
-  res.send('Hello World');
+app.use(helmet());
+app.use(serveStatic('public'));
+
+app.use((req, res, next) => {
+  console.log('middleware');
+  next();
 });
 
-app.listen(3000, function () {
+app.get('/', function(req, res) {
+  res.send('Hello World 3');
+});
+
+app.get('/home', (req, res) => {
+  res.send('Hello Home');
+});
+
+app.get('/user/:id', (req, res) => {
+  console.log(req.params.id);
+  res.send('user id: ' + req.params.id);
+});
+
+app.listen(3000, function() {
   console.log('start listen http://localhost:3000/');
 });
